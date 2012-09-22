236a1772c0021e7eddf6486dd0f048bc43daf8c6
Aaron Kling <akling8467@students.pcci.edu>
2012-09-21 21:54:16 -0500

    Fix merge again.

--- Statistics.aspx.cs
+++ Statistics.aspx.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
