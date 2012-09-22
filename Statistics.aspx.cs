0d13a0fc317bb340a903cee92366b63faa8c94d7
Aaron Kling <webgeek1234@gmail.com>
2012-09-21 20:47:51 -0500

    Revert "Commit for work later."
    This reverts commit d28f227432ce0b3684f3c3f86ccac1358d24edbd.

--- Statistics.aspx.cs
+++ Statistics.aspx.cs
@@ -9,8 +9,6 @@ public partial class Statistics : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        //ddlTeacher.DataBind();
-        //ddlCourses.DataBind();
-        Response.Write(gdvStats.Rows.Count.ToString());
+
     }
 }
